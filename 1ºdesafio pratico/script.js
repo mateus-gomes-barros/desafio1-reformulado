212eecacce1f9057212e8beb7f3a887856a8fe6e
Mateus Gomes barros <90994257+mateus-gomes-barros@users.noreply.github.com>
2021-12-17 18:46:35 -0300

    Add files via upload

---
+++ 1ºdesafio pratico/script.js
@@ -0,0 +1,14 @@
+
+
+//Nesta função, o formulário está sendo setado para ser visualizado, 
+//colocando uma class para retirar a opacitade 0 inicial para 1
+function model() {
+   
+    const form = document.querySelector(".model")
+
+ 
+    form.classList.toggle("visibilityModel")
+
+
+
+}
